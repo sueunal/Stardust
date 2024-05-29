ca571d19009b9fee3c5fe884df0375cb0b4419d1
sueunal <doms@kakao.com>
2024-05-29 21:53:23 +0900

    Initial Commit

---
+++ Stardust/StardustApp.swift
@@ -0,0 +1,32 @@
+//
+//  StardustApp.swift
+//  Stardust
+//
+//  Created by sueun kim on 5/29/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct StardustApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
