0feccffb2af8b54d0bcd6261c14e46fd1bf7c98a
sueunal <doms@kakao.com>
2024-06-08 20:44:44 +0900

    [correct]: Timer-> TimeManager 이름 변경

--- Stardust/View/TimerView.swift
+++ Stardust/View/TimerView.swift
@@ -6,42 +6,22 @@
 //
 
 import SwiftUI
+import Combine
 
 struct TimerView: View {
-    @State private var timeRemaining: TimeInterval = 0
-    @State private var timer: Timer?
+    @State private var currentTime: Date = Date()
+    let timer = TimageManager()
 
     var body: some View {
         VStack {
-            Text(timeString(from: timeRemaining))
+            Text("\(currentTime)")
                 .font(.system(size: 80))
                 .foregroundStyle(.white.opacity(0.8).gradient)
                 .padding()
         }
-        .onAppear{
-           startTimer()
-        }
-    }
-    
-    func startTimer() {
-        timer?.invalidate()
-        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
-            updateTimeRemaining()
-        }
-    }
-
-    func updateTimeRemaining() {
-        let now = Date()
-        let calendar = Calendar.current
-        let tomorrow = calendar.startOfDay(for: calendar.date(byAdding: .day, value: 1, to: now)!)
-        timeRemaining = tomorrow.timeIntervalSince(now)
-    }
-
-    func timeString(from timeInterval: TimeInterval) -> String {
-        let hours = Int(timeInterval) / 3600
-        let minutes = (Int(timeInterval) % 3600) / 60
-        let seconds = Int(timeInterval) % 60
-        return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+        .onReceive(timer.currentTimePublisher) { newCurrentTime in
+             self.currentTime = newCurrentTime
+           }
     }
 }
 
