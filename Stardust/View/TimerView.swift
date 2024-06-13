b728f3e93351a08b9c34e15432122ce4778ede07
sueunal <doms@kakao.com>
2024-06-13 23:55:00 +0900

    [feat]: TimerView 애니메이션 적용

--- Stardust/View/TimerView.swift
+++ Stardust/View/TimerView.swift
@@ -6,12 +6,13 @@
 //
 
 import SwiftUI
-import Combine
 
 struct TimerView: View {
     @StateObject var timer = TimerManager()
-    @State var isTimerOn: Bool = false
+    @State var isTimerState: Bool = false
     @State private var cheerUpMessage: String = "오늘도 한 걸음 나아갈 당신을 응원할게요!"
+    @State private var timerAnimationColor: [Color] = [.accentColor,.green,.orange]
+    
     @State var userInfo: Any? = nil
     var body: some View {
         VStack {
@@ -21,18 +22,21 @@ struct TimerView: View {
             Text("\(timer.remainingTime)")
                 .font(.system(size: 80))
                 .foregroundStyle(.white.opacity(0.8).gradient)
+                .scaleEffect(isTimerState ? 1 : 0.5)
                 .padding()
             Text(cheerUpMessage)
                 .font(AppFont.bodyBold)
                 .foregroundStyle(.white)
                 .transition(.identity)
-                .animation(.easeInOut(duration: 0.5), value: isTimerOn)
+                .animation(.easeInOut(duration: 0.5), value: isTimerState)
         }
-        .animation(.easeInOut(duration: 0.5), value: isTimerOn)
         .onAppear{
-            withAnimation{
-                isTimerOn = true
+            withAnimation(.easeIn(duration: 2)){
+                isTimerState = true
             }
+        }
+        .onDisappear{
+            isTimerState = false
         }
     }
 }
