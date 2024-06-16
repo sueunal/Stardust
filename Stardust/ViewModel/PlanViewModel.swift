f87f7c2dcf56ec1436b5c95f819fead1de0aa1ea
sueunal <doms@kakao.com>
2024-06-16 20:47:12 +0900

    [modifier]: PlanId저장 하기 위한 saveId 추가 - Userdefauls PlanId 저장 - ViewModel 초기화 시 기존 PlanId가 존재하는지 확인하고 PlanId를 서버에 요청함

--- Stardust/ViewModel/PlanViewModel.swift
+++ Stardust/ViewModel/PlanViewModel.swift
@@ -4,54 +4,93 @@
 //
 //  Created by sueun kim on 6/4/24.
 //
-
 import SwiftUI
 import Alamofire
 
-struct RequestModel: Codable {
+struct RequestModel: Codable,Identifiable {
+    let id = UUID()
     let PlanTitle: String
     let PlanDetail: [String]
     let PlanAttainment: [Bool]
     let Date: String
 }
+protocol SetGoal{
+    func createPlanId()
+    func requestPlans()
+    func addPlan(_ title: String, _ messages: [String], _ date: String)
+}
+
 struct ResponseModel: Codable{
     let id: String
 }
 
-class PlanViewModel: ObservableObject{
+class PlanViewModel: ObservableObject, SetGoal{
     @Published var plans: [Plan] = []
-    @Published var ToDo: RequestModel? = nil
+    @Published var ToDo: [RequestModel] = []
     let localURL: String = "http://localhost:12341"
     init(){
-        self.requestGet()
+        if let planId = UserDefaults.standard.string(forKey: "planID"){
+            return
+        }else{
+            self.createPlanId()
+        }
+    }
+    func createPlanId(){
+        let url = "\(localURL)/create"
+        AF.request(url,method: .post).responseDecodable(of: ResponseModel.self) { response in
+            switch response.result{
+            case .success(let data):
+                print("table id : \(data.id)")
+                self.saveId(data.id)
+            case .failure(let createError):
+                print("\(createError.localizedDescription)")
+            }
+        }
     }
     func addPlan(_ title: String, _ messages: [String], _ date: String){
-        let url = "\(localURL)/add"
+        guard let planID = UserDefaults.standard.string(forKey: "planID") else{
+            return
+        }
+        let url = "\(localURL)/add/\(planID)"
         let parameter: Parameters? = [
             "PlanTitle": title,
             "PlanDetail": messages,
             "PlanAttainment": [false],
             "Date": date
         ]
-        AF.request(url, method: .post, parameters: parameter, encoding: JSONEncoding.default).responseDecodable(of: ResponseModel.self){ response in
+        AF.request(url, method: .post, parameters: parameter, encoding: JSONEncoding.default).response{ response in
             switch response.result{
             case .success(let data):
-                print(data.id)
+                print(data)
             case .failure(let fail):
                 print(fail.localizedDescription)
             }
         }
     }
-    func requestGet(){
-        let url = "\(localURL)/get/e9e89f34-3fbd-44c6-954c-4abdfd5b7ee6"
-        AF.request(url, method: .get).responseDecodable(of: RequestModel.self) { response in
+    func requestPlans(){
+        guard let planID = UserDefaults.standard.string(forKey: "planID") else{
+           return
+        }
+        let url = "\(localURL)/get/\(planID)"
+        AF.request(url, method: .get).responseDecodable(of: [RequestModel].self) { response in
             switch response.result{
             case .success(let success):
-                print(success.PlanTitle)
                 self.ToDo = success
             case .failure(let fail):
                 print(fail.localizedDescription)
             }
         }
+    }
+    func saveId(_ saveIdString: String){
+       UserDefaults.standard.setValue(saveIdString, forKey: "planID")
+    }
+    func clearUserDefaults() {
+        let defaults = UserDefaults.standard
+        let dictionary = defaults.dictionaryRepresentation()
+        
+        for key in dictionary.keys {
+            defaults.removeObject(forKey: key)
+        }
+        defaults.synchronize()
     }
 }
