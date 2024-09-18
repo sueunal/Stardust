392097f6b3e77ffff1d1d4a66554aae414c5c1f4
sueun kim <doms@kakao.com>
2024-09-18 17:40:00 +0900

    [design]: 디자인 수정

--- Stardust/View/ToDo/ToDoListView.swift
+++ Stardust/View/ToDo/ToDoListView.swift
@@ -12,18 +12,19 @@ struct ToDoListView: View {
     @Environment(\.managedObjectContext) var viewContext
     @State private var isComplete: Bool = false
     let gardient = AngularGradient(colors: [.black, .white], center: .top)
+    let gridItem = Array(repeating: GridItem(.flexible()), count: 2)
     var body: some View {
         NavigationStack{
             ZStack{
                 BackgroundView()
                 VStack{
-                    Text("총 \(plans.count) 개!")
-                        .frame(maxWidth: .infinity,alignment: .leading)
+                    Text("오늘 달성할 목표: \(plans.count)")
+                        .frame(maxWidth: .infinity, alignment: .leading)
                         .font(AppFont.title1Bold)
                         .foregroundStyle(.white)
                     ScrollView{
-                        ForEach(plans){ plan in
-                            LazyVGrid(columns: [GridItem(.fixed(150)),GridItem(.fixed(150))]){
+                        LazyVStack{
+                            ForEach(plans){ plan in
                                 toDoItemView(plan)
                             }
                         }
@@ -35,14 +36,59 @@ struct ToDoListView: View {
     }
     @ViewBuilder
     func toDoItemView(_ plan: FetchedResults<Plan>.Element) -> some View{
-        VStack{
-            Text(plan.planTitle ?? "값을 불러올수없습니다.")
-            Text(plan.date ?? "값을 불러올수없습니다.")
+        VStack(alignment: .leading, spacing: 8){
+            HStack(alignment: .center){
+                VStack(alignment: .leading){
+                    Text("Design")
+                        .foregroundStyle(.blue)
+                    Text(plan.planTitle ?? "값을 불러올수없습니다.")
+                        .font(.title)
+                }
+                .padding(.trailing,2)
+                Spacer()
+                Image(systemName: "ellipsis")
+                    .padding(.horizontal,5)
+                    .padding(.vertical,9)
+                    .foregroundStyle(.black)
+                    .background(
+                        Circle()
+                            .stroke()
+                            .fill(.gray)
+                            .frame(width: 24,height: 24)
+                    )
+            }
+            .padding(.top, 14)
+            Text("Progress")
+                .font(.body)
+                .foregroundStyle(.gray)
+            ProgressView(value: 30, total: 100)
+                .frame(width: 144)
+                .frame(height: 4)
+            HStack(spacing: 6){
+                Text("30%")
+                    .font(.subheadline)
+            }
+            HStack{
+                Text(plan.date ?? "값을 불러올수없습니다.")
+                Spacer()
+                Text("High")
+                    .foregroundStyle(.white)
+                    .padding(.vertical,4)
+                    .padding(.horizontal,19)
+                    .background(
+                        RoundedRectangle(cornerRadius: 41)
+                            .fill(.blue)
+                    )
+            }
+            .padding(.bottom,14)
         }
-        .padding(300)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.horizontal,14)
+        .frame(height: 180)
         .background(
-            RoundedRectangle(cornerRadius: 10)
-                .fill(.white)
+            RoundedRectangle(cornerRadius: 11)
+                .stroke()
+                .fill(.gray)
         )
     }
     func dataToString(_ timestamp: Int)-> String{
